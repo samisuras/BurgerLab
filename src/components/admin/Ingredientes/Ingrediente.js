8fa8e5c4bf41c1b0fe4902771a777f2d192b1af1
samisuras <raulsamuelleos98@gmail.com>
2020-12-08 10:53:21 -0600

    ingredientes report

--- src/components/admin/Ingredientes/Ingrediente.js
+++ src/components/admin/Ingredientes/Ingrediente.js
@@ -5,7 +5,7 @@ import axios from "axios"
 class Ingrediente extends Component {
     constructor(props) {
         super(props);
-        this.state = { fecha: "", venta_dia: 0, venta_mensual: 0, venta_anual: 0 };
+        this.state = { fecha: "", ingrediente_dia: 0, ingrediente_mensual: 0, ingrediente_anual: 0 };
     }
 
     componentDidMount() {
@@ -18,9 +18,10 @@ class Ingrediente extends Component {
 
     enviar = async () => {
         if (this.state.fecha !== "") {
-            const respuesta = await axios.get("http://localhost:3300/reportes/venta/" + this.state.fecha)
+            const respuesta = await axios.get("http://localhost:3300/reportes/ingredientes/" + this.state.fecha)
             console.log(respuesta.data)
-            this.setState({ venta_dia: respuesta.data.venta_dia, venta_mensual: respuesta.data.venta_mensual, venta_anual: respuesta.data.venta_anual })
+            this.setState({ ingrediente_dia: respuesta.data.ingrediente_dia, ingrediente_mensual: respuesta.data.ingrediente_mes, ingrediente_anual: respuesta.data.ingrediente_anual })
+            console.log(this.state);
         } else {
             alert('Elija una fecha')
         }
@@ -42,7 +43,69 @@ class Ingrediente extends Component {
                                 <button onClick={this.enviar} className="btn btn-lg btn-outline-primary">Buscar</button>
                             </div>
                         </div>
-                        
+                        {this.state.ingrediente_anual !== 0 ?
+                            <div className="col-sm-12 pl-5 pr-5" align="center">
+                                <div className="row">
+                                    <div className="col-sm-4" align="center">
+                                        <h1 className="text-success">Ingredientes usados al Dia</h1>
+                                        <table class="table align-middle table-bordered mb-2">
+                                            <thead class="table-primary">
+                                                <th scope="col">Ingrediente</th>
+                                                <th scope="col">Cantidad</th>
+                                            </thead>
+                                            <tbody>
+                                                {this.state.ingrediente_dia.map((ingrediente, i) => {
+                                                    return [
+                                                        <td key={i}>{ingrediente.ingrediente}</td>,
+                                                        <td key={i}>{ingrediente.total}</td>,
+                                                        <tr></tr>
+                                                    ]
+                                                })}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div className="col-sm-4" align="center">
+                                        <h1 className="text-success">Ingredientes usados al Mes</h1>
+                                        <table class="table align-middle table-bordered mb-2">
+                                            <thead class="table-primary">
+                                                <th scope="col">Ingrediente</th>
+                                                <th scope="col">Cantidad</th>
+                                            </thead>
+                                            <tbody>
+                                                {this.state.ingrediente_mensual.map((ingrediente, i) => {
+                                                    return [
+                                                        <td key={i}>{ingrediente.ingrediente}</td>,
+                                                        <td key={i}>{ingrediente.total}</td>,
+                                                        <tr></tr>
+                                                    ]
+                                                })}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div className="col-sm-4" align="center">
+                                        <h1 className="text-success">Ingredientes usados al Año</h1>
+                                        <table class="table align-middle table-bordered mb-2">
+                                            <thead class="table-primary">
+                                                <th scope="col">Ingrediente</th>
+                                                <th scope="col">Cantidad</th>
+                                            </thead>
+                                            <tbody>
+                                                {this.state.ingrediente_anual.map((ingrediente, i) => {
+                                                    return [
+                                                        <td key={i}>{ingrediente.ingrediente}</td>,
+                                                        <td key={i}>{ingrediente.total}</td>,
+                                                        <tr></tr>
+                                                    ]
+                                                })}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                            :
+                            <span></span>
+                        }
                     </div>
                 </div>
             </span>
